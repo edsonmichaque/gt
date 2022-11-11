93db0fd166699f679d568c7d303939784a92969b
Edson Michaque <edson@michaque.com>
2022-11-11 20:39:13 +0200

    Clean up children processes before exiting

--- src/bin/gtd.c
+++ src/bin/gtd.c
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <sys/wait.h>
+#include <signal.h>
 
 #ifndef GT_WORKER
 #define GT_WORKERS 4
@@ -18,6 +19,16 @@ main(void)
 	for (int i = 0; i < GT_WORKERS; i++) {
 		if((pid[i] = fork()) == -1) {
 			perror("fork()");
+			fprintf(stderr, "Failed to create worker %d\n", i);
+			fprintf(stderr, "Killing all previouly created processes\n");
+
+			for (int j = 0; j < i; j++) {
+				if ((kill(pid[j], SIGKILL)) == -1) {
+					perror("kill()");
+				}
+			}
+
+			exit(EXIT_FAILURE);
 		}
 
 		if (pid[i] == 0) {
@@ -28,12 +39,12 @@ main(void)
 	}
 	
 	for (int i = 0; i < GT_WORKERS; i++) {
+		int status;
 		pid_t child_pid;
 		if ((child_pid = wait(&status)) == -1) {
 			perror("wait()");
 		}
 	
-		int status;
 		if (WIFEXITED(status)) {
 			int exit_status = WEXITSTATUS(status);
 			printf("worker %d exited with %d\n", child_pid, exit_status); 
